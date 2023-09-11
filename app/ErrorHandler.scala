c5538ba6190a79545f4e1e2bf37f7fa60cc2d3e3
Vuk Radmilovic <2001vuk@gmail.com>
2023-09-11 08:32:51 +0200

    Return only InternalServerError and log the exception when error is caught at top level

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -3,12 +3,13 @@ import play.api.http.HttpErrorHandler
 import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.Logging
 
 import scala.concurrent._
 import javax.inject.Singleton
 
 @Singleton
-class ErrorHandler extends HttpErrorHandler {
+class ErrorHandler extends HttpErrorHandler with Logging {
   def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     Future.successful(
       Status(statusCode)(Json.obj("message" -> s"A client error occurred: {$message}"))
@@ -17,8 +18,10 @@ class ErrorHandler extends HttpErrorHandler {
 
   def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     exception match {
-      case _: ValidationException => Future.successful(BadRequest(exception.getMessage))
-      case _ => Future.successful(InternalServerError(exception.getMessage))
+      case _: ValidationException => Future.successful(BadRequest(Json.obj("message" -> exception.getMessage)))
+      case _ =>
+        logger.error("Error caught in global error handler", exception)
+        Future.successful(InternalServerError)
     }
   }
 }
